fafdd8ff4abaac5e2ff7fe0f7c5b97fdafb29e0e
Jacopo Moioli <me@jacopomoioli.com>
2025-09-05 00:10:18 +0200

    updated readme and initial implementation of multi-provider support (only process seems to work tho)

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ GUID `22FB2CD6-0E7B-422B-A0C7-2FAD1FD0E716`
 - Thread Start (event 3): `[TID 14088] Thread start, created by TID 728, executing 0x7ffaca72afe0`
 - Thread Stop (event 4): `[TID 14088] Thread stop`
 
+## Build
+In order to build the project is enough to run 
+```bash
+cc main.c -o main.exe
+```
+via Msys2 (tested using CLANGARM64)
+
+## Acknowledgements
+
+- Pavel Yosifovch for creating [ETW Explorer](https://github.com/zodiacon/EtwExplorer), useful to avoid the hot garbage that the official documentation about ETW providers is
--- main.c
+++ main.c
@@ -11,7 +11,8 @@ typedef struct {
     char session_name[64];
 } ETW_SESSION_CONFIG;
 
-/* Windows kernel process provider event data */
+/* Windows kernel process provider */
+static const GUID KERNEL_PROCESS_PROVIDER_GUID = {0x22FB2CD6, 0x0E7B, 0x422B, {0xA0, 0xC7, 0x2F, 0xAD, 0x1F, 0xD0, 0xE7, 0x16}};
 
 /* ThreadStart (3) event data*/
 typedef struct {
@@ -27,41 +28,66 @@ typedef struct {
     ULONG SubProcessTag;
 } THREAD_START_DATA;
 
-/*
- *  Windows Kernel Process ETW producer GUID
- *  PS command to list all providers: logman query providers
- */
-static const GUID KERNEL_PROCESS_PROVIDER_GUID = {0x22FB2CD6, 0x0E7B, 0x422B, {0xA0, 0xC7, 0x2F, 0xAD, 0x1F, 0xD0, 0xE7, 0x16}};
+/* Windows kernel file provider*/
+static const GUID KERNEL_FILE_PROVIDER_GUID = {0xEDD08927, 0x9CC4, 0x4E65, {0xB9, 0x70, 0xC2, 0x56, 0x0F, 0xB5, 0xC2, 0x89}};
+
+typedef struct {
+    ULONG64 Irp;
+    ULONG64 FileObject;
+    ULONG ProcessId;
+    ULONG ThreadId;
+    ULONG CreateOptions;
+    ULONG CreateDisposition;
+    ULONG FileAttributes;
+    ULONG ShareAccess;
+    ULONG64 FileName;
+} FILE_CREATE_DATA;
 
 static ULONG target_pid;
 
+int compare_GUIDs(const GUID* guid1, const GUID* guid2){
+    return memcmp(guid1, guid2, sizeof(GUID)) == 0;
+}
 
 void WINAPI event_callback(PEVENT_RECORD event_record) {
     if(event_record->EventHeader.ProcessId != target_pid){
         return;
     }
-
-    switch(event_record->EventHeader.EventDescriptor.Id){
-        case 7:
-        case 8:
-        case 9:
-        case 21:
-        case 5:
-        case 6:
-            return;
-        case 3:
-            // Thread start
-            THREAD_START_DATA* data = (THREAD_START_DATA*)event_record->UserData;
-            printf("[TID %lu] Thread start, created by TID %lu, executing 0x%llx\n", data->ThreadId, event_record->EventHeader.ThreadId, data->Win32StartAddr);
-            break;
-        case 4:
-            // Thread stop
-            printf("[TID %lu] Thread stop\n", event_record->EventHeader.ThreadId);
-            break;
-        default:
-            printf("[TID %lu] Unknown (Event %lu)\n", event_record->EventHeader.ThreadId, event_record->EventHeader.EventDescriptor.Id);
+    if(compare_GUIDs(&event_record->EventHeader.ProviderId, &KERNEL_PROCESS_PROVIDER_GUID)){
+        switch(event_record->EventHeader.EventDescriptor.Id){
+            case 7:
+            case 8:
+            case 9:
+            case 21:
+            case 5:
+            case 6:
+                return;
+            case 3:
+                // Thread start
+                THREAD_START_DATA* data = (THREAD_START_DATA*)event_record->UserData;
+                printf("[TID %lu] Thread start, created by TID %lu, executing 0x%llx\n", data->ThreadId, event_record->EventHeader.ThreadId, data->Win32StartAddr);
+                break;
+            case 4:
+                // Thread stop
+                printf("[TID %lu] Thread stop\n", event_record->EventHeader.ThreadId);
+                break;
+            default:
+                printf("[TID %lu] Unknown (Event %lu)\n", event_record->EventHeader.ThreadId, event_record->EventHeader.EventDescriptor.Id);
+                break;
+        }
+    }else if(compare_GUIDs(&event_record->EventHeader.ProviderId, &KERNEL_FILE_PROVIDER_GUID)){
+        switch(event_record->EventHeader.EventDescriptor.Id){
+            case 12:
+                FILE_CREATE_DATA* data = (FILE_CREATE_DATA*)event_record->UserData;
+                printf("[TID %lu] Created file %s\n", event_record->EventHeader.ThreadId, data->FileName);
+                break;
+            default:
+                printf("[TID %lu] Unknown (Event %lu)\n", event_record->EventHeader.ThreadId, event_record->EventHeader.EventDescriptor.Id);
+                break;
+        }
     }
 }
+
 
 int main(int argc, char** argv){
     ETW_SESSION_CONFIG config = {0};
@@ -90,7 +116,7 @@ int main(int argc, char** argv){
             return 1;
     }
 
-    // Enable provider
+    // Enable process provider
     result = EnableTraceEx2(
         session_handle,
         &KERNEL_PROCESS_PROVIDER_GUID,
@@ -103,7 +129,24 @@ int main(int argc, char** argv){
     );
 
     if (result != ERROR_SUCCESS){
-        printf("[!] Failed to enable producer: error %d\n", result);
+        printf("[!] Failed to enable process provider: error %d\n", result);
+        return 1;
+    } 
+
+    // Enable file provider
+    result = EnableTraceEx2(
+        session_handle,
+        &KERNEL_FILE_PROVIDER_GUID,
+        EVENT_CONTROL_CODE_ENABLE_PROVIDER,
+        TRACE_LEVEL_VERBOSE,
+        0xFFFFFFFFFFFFFFFF, // all keywords
+        0,
+        0,
+        NULL
+    );
+
+    if (result != ERROR_SUCCESS){
+        printf("[!] Failed to enable process provider: error %d\n", result);
         return 1;
     } 
 
